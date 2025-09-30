05903cb2e14e9392d433b7fcb5df987972fb0f9a
enitrat <msaug@protonmail.com>
2025-09-30 14:30:23 +0200

    add analytics

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'msaug.dev';
-export const SITE_DESCRIPTION = 'Mathieu - Software Engineer. Explore my blog and projects. Multiple years of experience in software engineering, ZK, and LLM apps.';
+export const SITE_DESCRIPTION =
+	'Mathieu - Software Engineer. Explore my blog and projects. Multiple years of experience in software engineering, ZK, and LLM apps.';
