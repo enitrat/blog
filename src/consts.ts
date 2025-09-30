d5654587f338dfb49b51c4d67dd3757a3bf10e26
enitrat <msaug@protonmail.com>
2025-09-30 09:54:24 +0200

    update description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'msaug.dev';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Mathieu - Software Engineer. Explore my blog and projects. Multiple years of experience in software engineering, ZK, and LLM apps.';
